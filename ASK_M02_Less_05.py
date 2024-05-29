248e32e53e6d7132136a49a41d08e6c0c15959a4
kaskor-z <kaskor.z@yandex.ru>
2024-05-30 00:01:40 +0700

    Add files via upload

---
+++ ASK_M02_Less_05.py
@@ -0,0 +1,18 @@
+def get_matrix(n, m, value):
+    matrix = []
+    if value <= 0:
+        return matrix
+    internal_line = []
+    for i in range(n):
+        internal_line.clear()
+        for j in range(m):
+            internal_line.append(value)
+        matrix.append(internal_line)
+    return matrix
+
+
+x = int(input('введите колличество строк в матрице = '))
+y = int(input('введите колличество столбцов в матрице = '))
+z = int(input('введите значение элемента в матрице = '))
+new_matrix = get_matrix(x, y, z)
+print("\nРезультат заполнения Матрицы = ", new_matrix)
