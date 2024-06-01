0d7364e3417701ed8649c3ba1ad7043579915359
kaskor-z <kaskor.z@yandex.ru>
2024-06-01 23:16:53 +0700

    Update ASK_M02_Less_05.py

--- ASK_M02_Less_05.py
+++ ASK_M02_Less_05.py
@@ -11,8 +11,14 @@ def get_matrix(n, m, value):
     return matrix
 
 
-x = int(input('введите колличество строк в матрице = '))
-y = int(input('введите колличество столбцов в матрице = '))
-z = int(input('введите значение элемента в матрице = '))
-new_matrix = get_matrix(x, y, z)
-print("\nРезультат заполнения Матрицы = ", new_matrix)
+# x = int(input('введите колличество строк в матрице = '))
+# y = int(input('введите колличество столбцов в матрице = '))
+# z = int(input('введите значение элемента в матрице = '))
+# new_matrix = get_matrix(x, y, z)
+# print("\nРезультат заполнения Матрицы = ", new_matrix)
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
